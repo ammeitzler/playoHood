a14c485fde7331068c2ba8435ef21dee94f83260
Angeline <ammeitzler@gmail.com>
2016-01-30 16:17:11 -0500

    Added google maps API.

--- public/test.js
+++ public/test.js
@@ -1,3 +1,26 @@
-// alert('WHADDUPPPPP');
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.737333, lng: -73.992685},
+    zoom: 12
+  });
 
-console.log('hello halfkhal;kfda;');
+  var rectangle = new google.maps.Rectangle({
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    map: map,
+    bounds: {
+      north: 40.737333,
+      south: -73.992685,
+      east: 40.737333,
+      west: -73.992685
+    }
+  });
+
+
+
+
+}
