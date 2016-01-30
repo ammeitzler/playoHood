71051b7f255a01987f9f4a22dde6847bfd7edb19
ashleytqy <ashleytqy@gmail.com>
2016-01-30 15:45:00 -0500

    first commit :)

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+var db 
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds061721.mongolab.com:61721/locationizer', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, function(){
+		console.log('listening on 3000')
+	})
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get('/', function (req, res) {
+	res.sendFile( __dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
+
