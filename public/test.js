6ed522ade35b90055280d7bc54e394f689789826
Ariella Centlivre <ariellacentlivre@gmail.com>
2016-01-30 18:09:47 -0500

    flatiron district defined

--- public/test.js
+++ public/test.js
@@ -1,26 +1,56 @@
-var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.737333, lng: -73.992685},
-    zoom: 12
+    zoom: 15,
+    center: {lat: 40.737740, lng: -73.988192},
+    mapTypeId: google.maps.MapTypeId.TERRAIN
   });
 
-  var rectangle = new google.maps.Rectangle({
+  // Define the LatLng coordinates for the polygon.
+  var triangleCoords = [
+      {lat: 40.741004, lng: -73.994158},
+      {lat: 40.744800, lng: -73.991393},
+      {lat: 40.742085, lng: -73.984992},
+      {lat: 40.738354, lng: -73.987663}
+  ];
+
+  // Construct the polygon.
+  var bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
-    strokeWeight: 2,
+    strokeWeight: 3,
     fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    map: map,
-    bounds: {
-      north: 40.737333,
-      south: -73.992685,
-      east: 40.737333,
-      west: -73.992685
-    }
+    fillOpacity: 0.35
   });
+  bermudaTriangle.setMap(map);
 
+  // Add a listener for the click event.
+  bermudaTriangle.addListener('click', showArrays);
 
-
-
+  infoWindow = new google.maps.InfoWindow;
 }
+
+/** @this {google.maps.Polygon} */
+function showArrays(event) {
+  // Since this polygon has only one path, we can call getPath() to return the
+  // MVCArray of LatLngs.
+  var vertices = this.getPath();
+
+  var contentString = '<b>Bermuda Triangle polygon</b><br>' +
+      'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+      '<br>';
+
+  // Iterate over the vertices.
+  for (var i =0; i < vertices.getLength(); i++) {
+    var xy = vertices.getAt(i);
+    contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
+        xy.lng();
+  }
+
+  // Replace the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(event.latLng);
+
+  infoWindow.open(map);
+}
+
