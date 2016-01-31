7de39ad0de0bb1c86dd373c54fe42fa5e41db87a
Christopher Lee <cristovao.m.lee@gmail.com>
2016-01-30 19:56:37 -0500

    find by location working

--- public/test.js
+++ public/test.js
@@ -5,14 +5,14 @@ var map;
       zoom: 12
     });
 
-    var triangleCoords = [
-      {lat: 40.741004, lng: -73.994158},
-      {lat: 40.744800, lng: -73.991393},
-      {lat: 40.742085, lng: -73.983372},
-      {lat: 40.738882, lng: -73.985217},
-      {lat: 40.739492, lng: -73.986826},
-      {lat: 40.738354, lng: -73.987663}
-  ];
+      var triangleCoords = [
+     {lat: 40.739822, lng: -73.995070},
+     {lat: 40.744800, lng: -73.991393},
+     {lat: 40.742085, lng: -73.983372},
+     {lat: 40.738882, lng: -73.985217},
+     {lat: 40.739492, lng: -73.986826},
+     {lat: 40.736522, lng: -73.989019}
+ ];
 
   var triangleCoords2 = [
      {lat: 40.728328, lng: -74.0026868},
@@ -69,16 +69,83 @@ bermudaTriangle3.setMap(map);
 
   bermudaTriangle3.addListener('click', showArrays);
 
+
+  [].forEach.call( document.querySelectorAll( 'button' ), function ( a ) {
+    a.addEventListener( 'click', function () {
+
+      function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+
+        var latlng = new google.maps.LatLng(latitude, longitude);
+
+
+        if(google.maps.geometry.poly.containsLocation(latlng, bermudaTriangle)){
+          showOtherArrays({id: bermudaTriangle.id, latLng: latlng, getPath: bermudaTriangle.getPath() })
+        }
+
+        if(google.maps.geometry.poly.containsLocation(latlng, bermudaTriangle2)){
+          showOtherArrays({id: bermudaTriangle2.id, latLng: latlng, getPath: bermudaTriangle2.getPath() })
+        }
+
+        if(google.maps.geometry.poly.containsLocation(latlng, bermudaTriangle3)){
+          showOtherArrays({id: bermudaTriangle3.id, latLng: latlng, getPath: bermudaTriangle3.getPath() })
+        }
+
+        console.log('ACTUALLy CONTAINS', google.maps.geometry.poly.containsLocation(latlng, bermudaTriangle) );
+
+        dropPin({lat: latitude, lng: longitude}, map);
+      };
+
+      function error() {
+        output.innerHTML = "Unable to retrieve your location";
+      };
+
+
+
+      navigator.geolocation.getCurrentPosition(success, error);
+
+      
+    }, false );
+  });
+
   infoWindow = new google.maps.InfoWindow;
 }
+
+
 
 /** @this {google.maps.Polygon} */
 function showArrays(event) {
   // Since this polygon has only one path, we can call getPath() to return the
   // MVCArray of LatLngs.
   var vertices = this.getPath();
+  console.log('the gotten path', this.getPath());
   console.log('THIS', this.id);
+  console.log('event.latLng', event.latLng);
 
+    var triangleCoords = [
+     {lat: 40.739822, lng: -73.995070},
+     {lat: 40.744800, lng: -73.991393},
+     {lat: 40.742085, lng: -73.983372},
+     {lat: 40.738882, lng: -73.985217},
+     {lat: 40.739492, lng: -73.986826},
+     {lat: 40.736522, lng: -73.989019}
+ ];
+
+  var bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: '#6da89d',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#8bd5c8',
+    fillOpacity: 0.35,
+    id: 1, 
+  });
+
+  var kewl = new google.maps.LatLng(40.7399651, -73.99396349999999);
+
+  console.log('CONTAINS LOCATION', google.maps.geometry.poly.containsLocation(kewl, bermudaTriangle) );
   
   // var contentString = '<b>Flatiron District </b><br>' +
   //     'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
@@ -132,6 +199,72 @@ function showArrays(event) {
 
 
 
+function showOtherArrays(obj) {
+  // Since this polygon has only one path, we can call getPath() to return the
+  // MVCArray of LatLngs.
+  var vertices = obj.getPath;
+  
+  // var contentString = '<b>Flatiron District </b><br>' +
+  //     'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+  //     '<br>';
+
+  //var contentString = "<iframe src='https://embed.spotify.com/?uri=spotify:user:spotifycommunity:playlist:6JZYbH5F02DSJcJkkvKAPw' frameborder='0' allowtransparency='true' width='300' height='315'></iframe>"
+  //setPlaylist()
+
+  var contentString = setPlaylist(obj.id);
+  console.log('contentString', contentString);
+
+  // Iterate over the vertices.
+  // for (var i =0; i < vertices.getLength(); i++) {
+  //   var xy = vertices.getAt(i);
+  //   contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
+  //       xy.lng();
+  // }
+  var request = new XMLHttpRequest();
+  request.open('GET', '/areas/' + obj.id, true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var resp = request.responseText;
+      var updatedUrl = 'https://embed.spotify.com/?uri=spotify:' + JSON.parse(resp).playlistId;
+      console.log(JSON.parse(resp).playlistId);
+      // document.querySelectorAll('iframe')[0].setAttribute('src', updatedUrl);
+      // Replace the info window's content and position.
+      var contentString = "<iframe src=" + updatedUrl  + " frameborder='0' allowtransparency='true' width='300' height='315'></iframe>"
+
+
+      infoWindow.setContent(contentString);
+      infoWindow.setPosition(obj.latLng);
+
+      infoWindow.open(map);
+
+    } else {
+      // We reached our target server, but it returned an error
+
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+    console.log('there was an error');
+  };
+
+  request.send();  
+
+}
+
+
+
+function dropPin(latlng, map){
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: 'You are here'
+  });
+}
+
+
 function setPlaylist(id){
   console.log('noice')
   var request = new XMLHttpRequest();
@@ -160,17 +293,11 @@ function setPlaylist(id){
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  [].forEach.call( document.querySelectorAll( 'button' ), function ( a ) {
-    a.addEventListener( 'click', function () {
-        // code here
-        console.log(a.getAttribute('data-id'));
-        var playlistId = a.getAttribute('data-id');
-        setPlaylist(playlistId);
-    }, false );
-  });
-
 
 });
+
+// Latitude is 40.7399651° 
+// Longitude is -73.99396349999999°
 
 
 // navigator.geolocation.getCurrentPosition(function(position) {
